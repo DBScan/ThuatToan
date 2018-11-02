41aad70a2a1f614e0214c8c2f94cd8ae674c46f7
Nguyen Thanh Quang <quang2172@gmail.com>
2018-11-02 23:21:22 +0700

    Demo DbScan C#

---
+++ DBScan/DBScan/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace DBScan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLocate = new System.Windows.Forms.Label();
+            this.txtPoint = new System.Windows.Forms.Label();
+            this.pnlContainer = new System.Windows.Forms.Panel();
+            this.Eps = new System.Windows.Forms.Label();
+            this.txtEps = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboMinPts = new System.Windows.Forms.ComboBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLocate
+            // 
+            this.txtLocate.AutoSize = true;
+            this.txtLocate.Location = new System.Drawing.Point(12, 715);
+            this.txtLocate.Name = "txtLocate";
+            this.txtLocate.Size = new System.Drawing.Size(0, 17);
+            this.txtLocate.TabIndex = 0;
+            // 
+            // txtPoint
+            // 
+            this.txtPoint.AutoSize = true;
+            this.txtPoint.Location = new System.Drawing.Point(12, 510);
+            this.txtPoint.Name = "txtPoint";
+            this.txtPoint.Size = new System.Drawing.Size(0, 17);
+            this.txtPoint.TabIndex = 1;
+            // 
+            // pnlContainer
+            // 
+            this.pnlContainer.BackColor = System.Drawing.Color.White;
+            this.pnlContainer.Location = new System.Drawing.Point(12, 12);
+            this.pnlContainer.Name = "pnlContainer";
+            this.pnlContainer.Size = new System.Drawing.Size(1158, 495);
+            this.pnlContainer.TabIndex = 2;
+            this.pnlContainer.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pnlContainer_MouseClick);
+            this.pnlContainer.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlContainer_MouseMove);
+            // 
+            // Eps
+            // 
+            this.Eps.AutoSize = true;
+            this.Eps.Location = new System.Drawing.Point(12, 529);
+            this.Eps.Name = "Eps";
+            this.Eps.Size = new System.Drawing.Size(32, 17);
+            this.Eps.TabIndex = 3;
+            this.Eps.Text = "Eps";
+            // 
+            // txtEps
+            // 
+            this.txtEps.Location = new System.Drawing.Point(50, 526);
+            this.txtEps.Name = "txtEps";
+            this.txtEps.Size = new System.Drawing.Size(121, 22);
+            this.txtEps.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(180, 529);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "MinPts";
+            // 
+            // cboMinPts
+            // 
+            this.cboMinPts.FormattingEnabled = true;
+            this.cboMinPts.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cboMinPts.Location = new System.Drawing.Point(232, 526);
+            this.cboMinPts.Name = "cboMinPts";
+            this.cboMinPts.Size = new System.Drawing.Size(121, 24);
+            this.cboMinPts.TabIndex = 6;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(371, 526);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(92, 24);
+            this.btnRun.TabIndex = 7;
+            this.btnRun.Text = "Chạy";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(469, 526);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(92, 24);
+            this.btnXoa.TabIndex = 8;
+            this.btnXoa.Text = "Clear";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.cboMinPts);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEps);
+            this.Controls.Add(this.Eps);
+            this.Controls.Add(this.pnlContainer);
+            this.Controls.Add(this.txtPoint);
+            this.Controls.Add(this.txtLocate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "DbScan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtLocate;
+        private System.Windows.Forms.Label txtPoint;
+        private System.Windows.Forms.Panel pnlContainer;
+        private System.Windows.Forms.Label Eps;
+        private System.Windows.Forms.TextBox txtEps;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboMinPts;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnXoa;
+    }
+}
+
---
+++ DBScan/DBScan/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBScan
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        SolidBrush brush = new SolidBrush(Color.Black);
+        List<MyPoint> lst = new List<MyPoint>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = pnlContainer.CreateGraphics();
+            
+        }
+
+        private void pnlContainer_MouseClick(object sender, MouseEventArgs e)
+        {
+            g.FillEllipse(brush, new Rectangle(e.Location, new Size(10, 10)));
+            //g.DrawEllipse(Pens.Black, new Rectangle(e.Location, new Size(10, 10)));
+            MyPoint p = new MyPoint(e.Location, false, false);
+            
+            lst.Add(p);
+
+            txtPoint.Text += "(" + e.Location.X + ", " + e.Location.Y + ") ";
+        }
+
+        private void pnlContainer_MouseMove(object sender, MouseEventArgs e)
+        {
+            txtLocate.Text = "X: " + e.Location.X + ", Y: " + e.Location.Y;
+        }
+
+        private void DBScan(List<MyPoint> lst, float Eps, int MinPts, Graphics g, Brush brush)
+        {
+            List<Color> lstColor = new List<Color>();
+            lstColor.Add(Color.Red);
+            lstColor.Add(Color.Green);
+            lstColor.Add(Color.Blue);
+            lstColor.Add(Color.Yellow);
+            lstColor.Add(Color.Orange);
+            lstColor.Add(Color.Pink);
+            lstColor.Add(Color.Purple);
+            lstColor.Add(Color.Magenta);
+            lstColor.Add(Color.Lime);
+            lstColor.Add(Color.PowderBlue);
+            lstColor.Add(Color.Gray);
+            lstColor.Add(Color.PaleVioletRed);
+            lstColor.Add(Color.Peru);
+            lstColor.Add(Color.YellowGreen);
+            int k = 0;
+
+            List<MyPoint> C;
+            foreach (MyPoint p in lst)
+            {
+                if (!p.visited)
+                {
+                    p.visited = true;
+                    List<MyPoint> sphere_points = regionQuery(p, lst, Eps);
+                    if (sphere_points.Count < MinPts)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        C = new List<MyPoint>();
+                        expandCluster(lst, p, sphere_points, C, Eps, MinPts, g, brush, lstColor[k]);
+                        if (k != lstColor.Count - 1)
+                            k++;
+                    }
+                }               
+            }
+        }
+
+        private void expandCluster(List<MyPoint> lst, MyPoint p, List<MyPoint> sphere_points, List<MyPoint> C, float Eps, int MinPts, Graphics g, Brush brush, Color color)
+        {
+            brush = new SolidBrush(color);
+
+            C.Add(p);
+            p.isChild = true;
+            for (int i = 0; i < sphere_points.Count; i++)
+            {
+                if (!sphere_points[i].visited)
+                {
+                    sphere_points[i].visited = true;
+                    List<MyPoint> sphere_points_1 = regionQuery(sphere_points[i], lst, Eps);
+                    if (sphere_points_1.Count >= MinPts)
+                    {
+                        sphere_points.AddRange(sphere_points_1);
+                    }
+                    if (!sphere_points[i].isChild)
+                    {
+                        C.Add(sphere_points[i]);
+                        sphere_points[i].isChild = true;
+                        g.FillEllipse(brush, new Rectangle(sphere_points[i].vt, new Size(10, 10)));
+                        Thread.Sleep(100);
+                    }
+                }
+            }
+
+            
+                      
+            //for (int i = 0; i < C.Count; i++)
+            //{
+            //    
+            //}
+        }
+
+        private List<MyPoint> regionQuery(MyPoint p, List<MyPoint> lst, float Eps)
+        {
+            List<MyPoint> lstRegion = new List<MyPoint>();
+
+            for (int i = 0; i < lst.Count; i++)
+            {
+                if (distanceEuclide(p, lst[i]) <= Eps)
+                {
+                    lstRegion.Add(lst[i]);
+                }
+            }
+            return lstRegion;
+        }
+
+        private double distanceEuclide(MyPoint a, MyPoint b)
+        {
+            return Math.Sqrt(Math.Pow(a.vt.X - b.vt.X, 2) + Math.Pow(a.vt.Y - b.vt.Y, 2));
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            DBScan(lst, float.Parse(txtEps.Text), int.Parse(cboMinPts.Text), g, brush);
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            lst.Clear();
+        }
+    }
+
+    public class MyPoint
+    {
+        public Point vt { get; set; }
+        public bool visited { get; set; }
+        public bool isChild { get; set; }
+
+        public MyPoint() { }
+        public MyPoint(Point vt, bool visited, bool isChild)
+        {
+            this.vt = vt;
+            this.visited = visited;
+            this.isChild = isChild;
+        }
+    }
+}
